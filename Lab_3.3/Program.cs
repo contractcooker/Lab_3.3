3fdfc61864808eadc3cc159ba345a1049347046c
Tom Barron <tombarron@quickenloans.com>
2020-01-30 16:20:09 -0500

    code complete

--- Lab_3.3/Program.cs
+++ Lab_3.3/Program.cs
@@ -7,22 +7,33 @@ namespace Lab_3._3
     {
         static void Main(string[] args)
         {
-            string input, reverseInput="";
-            char[] charArray;
-            Stack<char> myStack = new Stack<char>();
+            string input;
+            
 
             Console.Write("Please enter a word you would like to reverse: ");
 
             input = Console.ReadLine();
-            charArray = input.ToCharArray();
+
+            Console.WriteLine("Your word reversed is: {0}",ReverseString(input)); 
+            
+        }
+
+        private static string ReverseString(string input)
+        {
+            Stack<char> myStack = new Stack<char>();
+
+            char[] charArray = input.ToCharArray();
             for (int i = 0; i < charArray.Length; i++)
             {
                 myStack.Push(charArray[i]);
             }
-            for (int i = myStack.Count; i > 0; i--)
+            for (int i = 0; i < charArray.Length; i++)
             {
-               reverseInput += myStack.Pop;
+                charArray[i] = myStack.Pop();
             }
+            string reversedInput = new string(charArray);
+
+            return reversedInput;
         }
     }
 }
